50a24d0428f5a29597f7385a406bcad1feb3473f
fanny <renatenyatsikor@gmail.com>
2024-10-04 11:50:58 +0200

    Add new task feature begin : UIFirst Part done

--- app/src/main/java/com/example/todoapp/ui/screens/NewTaskScreen.kt
+++ app/src/main/java/com/example/todoapp/ui/screens/NewTaskScreen.kt
@@ -1,11 +1,14 @@
 package com.example.todoapp.ui.screens
 
+import android.annotation.SuppressLint
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.MaterialTheme
@@ -17,33 +20,36 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.Placeholder
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 
 @Composable
 fun  NewTaskScreen() {
 
-
-
     Column(
-        verticalArrangement = Arrangement.SpaceBetween,
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
 
 
         modifier = Modifier
-            .padding(20.dp, 150.dp)
+            .padding(20.dp, 20.dp)
             .fillMaxSize()
-            .background(Color.White)
     ) {
 
         Text("New Tasks",
-            style = MaterialTheme.typography.headlineMedium
+            style = MaterialTheme.typography.headlineMedium,
+            modifier = Modifier.padding(0.dp, 5.dp)
         )
+        Spacer(Modifier.padding(15.dp))
        FirstPart()
+        Spacer(Modifier.padding(15.dp))
         DefineTime()
+        Spacer(Modifier.padding(15.dp))
         TaskCategories()
-
-
         //Create Task Button
     }
 
@@ -53,29 +59,55 @@ fun  NewTaskScreen() {
 
 
 //Task substask add details
+@SuppressLint("InvalidColorHexValue")
 @Composable
 fun FirstPart() {
     Column(
         modifier = Modifier
             .fillMaxWidth()
-            .background(Color.LightGray),
+            .background(
+                Color(0xFFf4f4f4),
+                shape = MaterialTheme.shapes.medium
+            )
+        ,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         OutlinedTextField(
             modifier = Modifier
-                .padding(5.dp)
-                .shadow( 0.dp, shape = MaterialTheme.shapes.medium)
-            ,
-            value = "Todo name",
+                .padding(8.dp)
+                .height(50.dp)
+                .fillMaxSize()
+                .shadow( 0.dp, shape = MaterialTheme.shapes.medium),
+            value = "", //Not Good need To change That
             onValueChange = {},
-            shape = MaterialTheme.shapes.medium
+            shape = MaterialTheme.shapes.medium,
+            textStyle = MaterialTheme.typography.bodySmall,
+            placeholder = {
+                Text( text ="Task name",
+                    style = MaterialTheme.typography.bodySmall,
+                    color = Color(0xFF9999a1)
+                )
+
+            }
         )
 
         OutlinedTextField(
-            modifier = Modifier.padding(5.dp),
-            value = "Add your task details",
+            modifier = Modifier
+                .padding(8.dp)
+                .height(120.dp)
+                .fillMaxSize()
+            ,
+            singleLine = false,
+            value = "",
             shape = MaterialTheme.shapes.medium,
-            onValueChange = {}
+            onValueChange = {},
+            placeholder = {
+                Text( text ="Add your task details",
+                    style = MaterialTheme.typography.bodySmall,
+                    color = Color(0xFF9999a1)
+                )
+
+            }
         )
 
     }
@@ -93,7 +125,6 @@ fun DefineTime() {
             .background(Color.Red)
     )
 }
-
 @Composable
 fun TaskCategories() {
     Box(
